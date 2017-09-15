7694b50466e6279bf1cfba32506cd7c7276e279a
dollyn <dollyn.sun@gmail.com>
2017-09-15 16:42:43 +0800

    init

--- file_utils.go
+++ file_utils.go
@@ -8,3 +8,10 @@ func DirectoryExists(path string) bool {
 	if os.IsNotExist(err) { return false }
 	return true
 }
+
+func FileExists(path string) bool{
+	_, err := os.Stat(path)
+	if err == nil { return true }
+	if os.IsNotExist(err) { return false }
+	return true
+}
